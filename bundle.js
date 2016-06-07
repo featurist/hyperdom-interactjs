bde8f0255a96318cf8d8dd31f9ec845626432275
Josh Chisholm <joshuachisholm@gmail.com>
2016-06-07 16:59:27 +0100

    updated example

--- bundle.js
+++ bundle.js
@@ -5,31 +5,88 @@ var h = plastiq.html;
 
 function render(model) {
   return h('.page',
-    interact.draggable({ inertia: true },
+    interact({ binding: [model, 'animal'], draggable: true },
       h('.red', 'Animal')
     ),
-    interact.draggable({
-      snap: { targets: [ interact.createSnapGrid({ x: 20, y: 20 }) ] } },
+    interact({
+        binding: [model, 'vegetable'],
+        draggable: {
+          snap: { targets: [ interact.createSnapGrid({ x: 20, y: 20 }) ] }
+        }
+      },
       h('.green', 'Vegetable')
     ),
-    interact.draggable(
-      interact.rotatable(
-        h('.blue', 'Mineral')
-      )
-    )
+    interact({
+        binding: [model, 'mineral'],
+        draggable: { inertia: true },
+        rotatable: true,
+        scalable: true
+      },
+      h('.blue', 'Mineral')
+    ),
+    h('pre', JSON.stringify(model, null, 2))
   );
 }
 
-plastiq.append(document.getElementById('example'), render, {});
+var model = {
+  animal:    { x: 0,  y: 0, scale: 1.1, rotation: 1  },
+  vegetable: { x: 30, y: 0, scale: 1.0, rotation: 0  },
+  mineral:   { x: 60, y: 0, scale: 1.1, rotation: -3 }
+}
+
+plastiq.append(document.getElementById('example'), render, model);
 
 },{"../":2,"plastiq":14}],2:[function(require,module,exports){
 var plastiq = require('plastiq');
 var interact = require('interact.js');
 
-var translateReg = /translate\((\d+)px,\s*(\d+)px\)/i;
-var rotateReg = /rotate\((\d+)deg\)/i;
+var translateReg = /translate\((-?[\d\.]+)px,\s*(-?[\d\.]+)px\)/i;
+var rotateReg = /rotate\((-?[\d\.]+)deg\)/i;
+var scaleReg = /scale\((-?[\d\.]+)\)/i;
 
-function dragMoveListener(event) {
+function plastiqInteractJs(options, vnode) {
+  var binding = plastiq.html.binding(options.binding);
+  var transform = binding.get();
+  return plastiq.html.component(
+    {
+      onadd: function (element) {
+        if (transform) {
+          element.style.transform = writeTransform(transform);
+          console.log("WRITING:", transform, writeTransform(transform));
+        }
+        if (options.draggable) {
+          var opts = (options.draggable === true) ?
+            {} : options.draggable || {};
+          opts.onmove = plastiq.html.refreshify(makeDragMoveListener(binding));
+          interact(element).draggable(opts);
+        }
+        if (options.rotatable || options.scalable) {
+          interact(element).gesturable({
+            onmove: plastiq.html.refreshify(makeGestureMoveListener(options, binding))
+          });
+        }
+      },
+      onupdate: function (element) {
+      }
+    },
+    vnode
+  );
+}
+module.exports = plastiqInteractJs;
+
+plastiqInteractJs.createSnapGrid = interact.createSnapGrid;
+
+function writeTransform(t) {
+  return "translate(" + t.x + "px," + t.y + "px) scale(" + t.scale + ") rotate(" + t.rotation + "deg)";
+}
+
+function makeDragMoveListener(binding) {
+  return function(event) {  
+    dragMoveListener(event, binding);
+  }
+}
+
+function dragMoveListener(event, binding) {
   var target = event.target;
   var x, y;
   var transform = target.style.transform || target.style.webkitTransform;
@@ -48,59 +105,71 @@ function dragMoveListener(event) {
   } else {
     target.style.webkitTransform = target.style.transform = transform + ' ' + newTranslate;
   }
+  
+  if (binding) {
+    var transform = binding.get() || {};
+    transform.x = x;
+    transform.y = y;
+    binding.set(transform);
+  }
 }
 
-function rotateMoveListener(event) {
+function makeGestureMoveListener(options, binding) {
+  return function(event) {
+    if (options.rotatable) {
+      rotateMoveListener(event, binding);
+    }
+    if (options.scalable) {
+      scaleMoveListener(event, binding);
+    }
+  }
+}
+
+function rotateMoveListener(event, binding, refresh) {
   var target = event.target;
-  var deg;
+  var rotation;
   var transform = target.style.transform || target.style.webkitTransform;
   var existing = transform.match(rotateReg);
   if (existing) {
-    deg = Number(existing[1]);
+    rotation = Number(existing[1]);
   } else {
-    deg = 0;
+    rotation = 0;
   }
-  deg += event.dx;
-  var newTranslate = 'rotate(' + deg + 'deg)';
+  rotation += event.da;
+  var newTranslate = 'rotate(' + rotation + 'deg)';
   if (existing) {
     target.style.webkitTransform = target.style.transform = transform.replace(rotateReg, newTranslate);
   } else {
     target.style.webkitTransform = target.style.transform = transform + ' ' + newTranslate;
   }
+
+  if (binding) {
+    var transform = binding.get() || {};
+    transform.rotation = rotation;
+  }
 }
 
-module.exports = {
-  draggable: function(options, element) {
-    if (!element) { element = options; }
-    options = options || {};
-    options.onmove = dragMoveListener;
-    return plastiq.html.component(
-      {
-        onadd: function (element) {
-          interact(element).draggable(options);
-        },
-        onupdate: function (element) {
-        }
-      },
-      element
-    );
-  },
-
-  createSnapGrid: interact.createSnapGrid,
-
-  rotatable: function(element) {
-    return plastiq.html.component(
-      {
-        onadd: function (element) {
-          interact(element.parentNode).gesturable({
-            onmove: rotateMoveListener
-          });
-        },
-        onupdate: function (element) {
-        }
-      },
-      element
-    );
+function scaleMoveListener(event, binding, refresh) {
+  var target = event.target;
+  var scale;
+  var transform = target.style.transform || target.style.webkitTransform;
+  var existing = transform.match(scaleReg);
+  if (existing) {
+    scale = Number(existing[1]);
+  } else {
+    scale = 1;
+  }
+  scale += event.ds;
+  var newTranslate = 'scale(' + scale + ')';
+  if (existing) {
+    target.style.webkitTransform = target.style.transform = transform.replace(scaleReg, newTranslate);
+  } else {
+    target.style.webkitTransform = target.style.transform = transform + ' ' + newTranslate;
+  }
+  
+  if (binding) {
+    var transform = binding.get() || {};
+    transform.scale = scale;
   }
 }
 
