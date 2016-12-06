be9a8b1c412059e47ee62a9e3393c6998473dd6a
Josh Chisholm <joshuachisholm@gmail.com>
2016-12-06 15:16:26 +0000

    plastiq -> hyperdom

--- example.js
+++ example.js
@@ -1,23 +1,5 @@
-<html>
-  <head>
-    <title>hyperdom-interactjs example</title>
-    <link href="example.css" type="text/css" rel="stylesheet" />
-  </head>
-  <body>
-    <h1>hyperdom-interactjs</h1>
-    <p>
-      A <a href="https://github.com/featurist/hyperdom">hyperdom</a> component that
-      enables multi-touch manipulation of elements using
-      <a href="https://github.com/taye/interact.js">interact.js</a>.
-    </p>
-    <a href="https://github.com/featurist/hyperdom-interactjs"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"></a>
-
-    <div id="example">
-    </div>
-
-    <pre>
 var hyperdom = require('hyperdom');
-var interact = require('hyperdom-interactjs');
+var interact = require('../');
 var h = hyperdom.html;
 
 function render(model) {
@@ -68,9 +50,4 @@ var model = {
   mineral:   { x: 60, y: 0, scale: 1.1, rotation: -3, moves: 0, gestures: 0 }
 }
 
-hyperdom.append(document.body, render, model);
-    </pre>
-
-    <script src="bundle.js"></script>
-  </body>
-</html>
+hyperdom.append(document.getElementById('example'), render, model);
